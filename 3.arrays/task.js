4cdc5cee14a6d86647ef8e8d8b0c4585b4454e32
Evgenii <mikhailovich21@mail.ru>
2023-11-21 23:53:31 +0500

    DZ3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item) => arr1[item] === arr2[item]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users
+    .filter(user => user.gender === gender)
+    .map(result => result.age)
+    .reduce((summ, userAge, index, arr) => {
+        summ += userAge;
+        if (index === arr.length - 1) {
+            return summ / arr.length;
+        }
+        return summ;
+    }, 0);
 }
